e79ce54fa1208fb5187bd174f7d5e1e832961a73
Worm <mwormley008@gmail.com>
2023-03-20 21:40:52 -0500

    First commit

---
+++ routes.py
@@ -0,0 +1,13 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+    
+
+@app.route('/test')
+def hello_test():
+    return render_template('favorites.html')
+
